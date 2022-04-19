ac168426fe232b090aea3462356df06bb5f33884
DHayes23 <D.Hayes23@hotmail.com>
2022-04-19 14:40:52 +0000

    Implement conditional nav/one-click logout

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,10 @@
 // The following function gets the current year appends it to the
 //  copyright message. The message is appended to the footer.
+
+$(document).on('click', '#signOutLink', function() {
+  $('#signOutBtn').click()
+});
+
 $(document).ready(function(){
     current_year = new Date().getFullYear()
     $('#copyright-text').html(`&copy; Valencia Tennis Club ${current_year}`);
