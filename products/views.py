cf41b39e4b52ec8396ff7ec94a248e5196dd26ee
DHayes23 <D.Hayes23@hotmail.com>
2022-05-01 00:29:42 +0000

    Fix linting errors

--- products/views.py
+++ products/views.py
@@ -3,13 +3,14 @@ from .models import Product
 
 # Create your views here.
 
+
 def all_products(request):
-    """ This view renders all products""" 
+    """ This view renders all products"""
 
     products = Product.objects.all()
 
     context = {
         'products': products,
     }
-    
+
     return render(request, 'products/products.html', context)
