09bf3c555368977c9765436127349312ff17bca9
DHayes23 <D.Hayes23@hotmail.com>
2022-05-04 21:34:43 +0000

    Remove secret key and set debug

--- valencia_tennis/settings.py
+++ valencia_tennis/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cjrzqoq377mzj5l+0zbz%*a_4e&+ew0n75dk6+#%wfmpfqzn63'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dh-milestone-project-4.herokuapp.com', 'localhost']
 
