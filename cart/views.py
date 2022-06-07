5c65a6d4b88d73d30315853320b4f1fe41a67856
DHayes23 <D.Hayes23@hotmail.com>
2022-06-07 16:45:20 +0000

    Add cart content, styles and urls

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def view_cart(request):
+    """ This view renders the contents of the user's cart.""" 
+    
+    return render(request, 'cart/cart.html')
