6480b50561980022a3af7a3c71735c1ee779a05b
DHayes23 <D.Hayes23@hotmail.com>
2022-06-16 22:30:59 +0000

    Set Debug to False

--- valencia_tennis/settings.py
+++ valencia_tennis/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = FALSE
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['dh-milestone-project-4.herokuapp.com', 'localhost']
 
