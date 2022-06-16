64a39332f4946632a2a22c89a1ddd3091e47f386
DHayes23 <D.Hayes23@hotmail.com>
2022-06-16 00:18:01 +0000

    Set debug to dynamic

--- valencia_tennis/settings.py
+++ valencia_tennis/settings.py
@@ -14,8 +14,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = FALSE
 
 ALLOWED_HOSTS = ['dh-milestone-project-4.herokuapp.com', 'localhost']
 
