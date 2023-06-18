c2084182415c326c8ef45a0e97f0bf1933d99a7d
DHayes23 <D.Hayes23@hotmail.com>
2023-06-18 18:58:53 +0000

    Set Debug to True

--- valencia_tennis/settings.py
+++ valencia_tennis/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = TRUE
 
 ALLOWED_HOSTS = ['dh-milestone-project-4.herokuapp.com', 'localhost']
 
